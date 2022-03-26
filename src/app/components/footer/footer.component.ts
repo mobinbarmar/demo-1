e135d47c7422df6b626df44513113e296f4b5d22
mobin <barmar.mobin2@gmail.com>
2022-03-26 19:05:07 +0430

    footer animated

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,4 +12,36 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  healthy:string = '1'
+  healthyEnter(){
+    this.healthy = '1.4'
+  }
+  healthyLeave(){
+    this.healthy = '1'
+  }
+
+  contact:string = '1'
+  contactEnter(){
+    this.contact = '1.4'
+  }
+  contactLeave(){
+    this.contact = '1'
+  }
+
+  ready:string = '1'
+  readyEnter(){
+    this.ready = '1.4'
+  }
+  readyLeave(){
+    this.ready = '1'
+  }
+
+  world:string = '1'
+  worldEnter(){
+    this.world = '1.4'
+  }
+  worldLeave(){
+    this.world = '1'
+  }
+
 }
