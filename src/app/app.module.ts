eac78f5b41ec5f6b814f40cbfacad1bb85cc2fcc
mobin <barmar.mobin2@gmail.com>
2022-03-26 10:29:05 +0430

    bootstrap added and modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ProductComponent } from './pages/product/product.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    CartComponent,
+    ProductComponent,
+    MainPageComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
