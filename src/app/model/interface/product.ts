b52d0f7097db6ddc4871a7cbdd2b5ce2cfd85f0d
faeze <faezeebold@gmail.com>
2022-03-29 12:45:35 +0430

    disin2

--- src/app/model/interface/product.ts
+++ src/app/model/interface/product.ts
@@ -2,4 +2,5 @@ export interface Product {
     title:string;
     description:string;
     price:String;
+    url:string;
 }
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -19,11 +19,13 @@ export class CartComponent implements OnInit {
       title:"پرینتر کانن مدل PIXMA MG2540s",
       description:"پرینتر کانن مدل PIXMA MG2540s",
       price:"8900000",
+      url:"../../../assets/img/kisspng-multi-function-printer-inkjet-printing-canon-autom-color-printer-5a77a6aec2c8c3.9515433815177908947978.png",
     },
     {
       title:"موس آرما مدل M27",
     description:"به همراه دانگل",
     price:"1000000",
+    url:"../../../assets/img/kisspng-computer-mouse-computer-keyboard-laptop-logitech-computer-mouse-5acaaed741a1d4.0967488915232324712688.png",
 
     }
 
