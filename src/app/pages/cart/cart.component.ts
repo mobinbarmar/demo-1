7ebefb34378a76ab15bd8f434599ad6be27d08ce
faeze <faezeebold@gmail.com>
2022-03-29 12:26:49 +0430

    initial design

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/interface/product';
 
 @Component({
   selector: 'app-cart',
@@ -10,6 +11,25 @@ export class CartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  flag:boolean = true;
+
+  informations :Product[] = [
+    {
+      title:"پرینتر کانن مدل PIXMA MG2540s",
+      description:"پرینتر کانن مدل PIXMA MG2540s",
+      price:"8900000",
+    },
+    {
+      title:"موس آرما مدل M27",
+    description:"به همراه دانگل",
+    price:"1000000",
+
+    }
+
+  ]
+  product(){
+    this.flag = false;
   }
 
 }
